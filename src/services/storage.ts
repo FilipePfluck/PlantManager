e2d9f1c6e10f1fb91f39ffe093ece126220c657f
Filipe Pfluck <filipepfluckdev@gmail.com>
2021-04-23 22:12:05 -0300

    day 5

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,18 +1,23 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { format, differenceInHours,getHours } from 'date-fns'
 
+import * as Notifications from 'expo-notifications'
+
 import { PlantProps } from '../interfaces/plant'
 import { getDifferenceInHours } from '../utils/getDifferenceInHours'
 
 export interface StoragePlantProps {
     [id: string]: {
-        data: PlantProps
+        data: PlantProps,
+        notificationId: string
     }
 }
 
 export async function deletePlant(id: string): Promise<void>{
     const data = await AsyncStorage.getItem('@plantmanager:plants')
     const plants = data ? (JSON.parse(data) as StoragePlantProps) : {}
+    
+    await Notifications.cancelScheduledNotificationAsync(plants[id].notificationId)
 
     delete plants[id]
 
@@ -39,14 +44,29 @@ export async function savePlant(plant: PlantProps): Promise<void>{
             Math.ceil((now.getTime() - nextTime.getTime()) / 1000)
         )
 
-            
+        const notificationId = await Notifications.scheduleNotificationAsync({
+            content: {
+                title: 'Heeey',
+                body: `Está na hora de cuidar da sua ${plant.name}`,
+                sound: true,
+                priority: Notifications.AndroidNotificationPriority.HIGH,
+                data: {
+                    plant
+                }
+            },
+            trigger: {
+                seconds: seconds < 60 ? 60 : seconds,
+                repeats: true
+            }
+        })
 
         const data = await AsyncStorage.getItem('@plantmanager:plants')
         const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {}
 
         const newPlant = {
             [plant.id]: {
-                data: plant
+                data: plant,
+                notificationId
             }
         }
 
