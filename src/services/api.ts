60918fa415d30138054700d3bb8ca211f3784cc9
Filipe Pfluck <filipepfluckdev@gmail.com>
2021-05-08 20:33:18 -0300

    json-server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.7:3333'
+    baseURL: 'https://my-json-server.typicode.com/FilipePfluck/PlantManager'
 })
 
 export default api
